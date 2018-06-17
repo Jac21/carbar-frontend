f2987bced1633a9152468ae71c092bd7d99cf6be
Jeremy Cantu <mail@jeremycantu.com>
2018-06-17 16:59:21 -0500

    Removed unecessary inital session check on app-load

--- src/js/App.js
+++ src/js/App.js
@@ -3,7 +3,7 @@ import { IntlProvider, addLocaleData } from 'react-intl';
 import en from 'react-intl/locale-data/en';
 import { getCurrentLocale, getLocaleData } from 'grommet/utils/Locale';
 import { Provider } from 'react-redux';
-import { initialize } from './actions/session';
+// import { initialize } from './actions/session';
 import store from './store';
 import Main from './components/Main';
 
@@ -17,9 +17,9 @@ try {
 }
 const localeData = getLocaleData(messages, locale);
 
-if (window.location.pathname !== '/login') {
-  store.dispatch(initialize(window.location.pathname));
-}
+// if (window.location.pathname !== '/login') {
+//   store.dispatch(initialize(window.location.pathname));
+// }
 
 export default () => (
   <Provider store={store}>
